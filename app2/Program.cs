a31dc92b055fe228f618f4587138b1117f15a90c
Artur Kociuba <arturkociuba@motorolasolutions.com>
2015-01-29 16:49:14 +0100

    nowa zmiana 3

--- app2/Program.cs
+++ app2/Program.cs
@@ -10,7 +10,7 @@ namespace app2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("zmiana na branchu task1");
+            Console.WriteLine("zmiana na branchu task1 jeszcze zmiana");
         }
     }
 }
